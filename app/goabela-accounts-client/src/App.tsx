98a43aa11ba2cd6c328667bf9c49eba47b6efb97
Bela Arany <git@goabela.com>
2020-03-13 13:15:36 +0100

    updates

---
+++ app/goabela-accounts-client/src/App.tsx
@@ -0,0 +1,5 @@
+import logo from "./logo.svg"
+import React from "react"
+import AuthView from "views/Auth/Auth"
+
+export default AuthView
---
+++ app/goabela-accounts-client/src/views/Auth/pages/Signin/Signin.tsx
@@ -0,0 +1,8 @@
+import React from "react"
+import Paper from "@material-ui/core/Paper"
+
+const AuthView: React.FC = (): JSX.Element => {
+	return <Paper>Hi there</Paper>
+}
+
+export default AuthView
